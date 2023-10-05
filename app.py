f4ee520d9b77cccf7bb9e19db7e95f9fae9d9142
Chris Williams <christopherlwilliams25@gmail.com>
2023-10-05 16:01:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from aws_cdk import core
+#!/usr/bin/env python3
+import aws_cdk as core
 
 from cdk_app.cdk_app_stack import HelloCdkStack
 
 app = core.App()
 HelloCdkStack(app, "CdkTestAppCICDContainer")
 app.synth()
+
