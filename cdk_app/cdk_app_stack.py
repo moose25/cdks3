a10dec24f66fbdd8d0469db6c16f368301b38432
Chris Williams <christopherlwilliams25@gmail.com>
2023-10-06 10:33:45 -0400

    Update cdk_app_stack.py

--- cdk_app/cdk_app_stack.py
+++ cdk_app/cdk_app_stack.py
@@ -1,9 +1,10 @@
-from aws_cdk import core
+from constructs import Construct
+from aws_cdk import App, Stack    
 from aws_cdk import aws_s3 as s3
 
-class HelloCdkStack(core.Stack):
+class HelloCdkStack(Stack):
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         bucket = s3.Bucket(self, "wzchrbuckettestcdk", versioned=True)
